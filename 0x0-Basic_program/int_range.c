ebf2e33aa8cb88955345533d7334b28cc55442e9
eliwarrie <eelitetech9@gmail.com>
2022-09-24 09:56:46 +0000

    range of signed and unsigned integers

---
+++ 0x0-Basic_program/int_range.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <limits.h>
+
+// Ranges of integers signed and unsigned
+
+int main()
+{
+    int var1 = INT_MIN;
+    int var2 = INT_MAX;
+
+    printf("Range of signed integer is from: %d to %d\n", var1, var2);
+
+    return (0);
+}
